9f43f58dcb5a74e3670f7a3dc2ad08813e03b521
junseok oh <ixtears23@icloud.com>
2022-06-20 00:25:05 +0900

    13장. 진짜로 만들기

--- src/test/java/personal/junseok/tdd/MoneyTest.java
+++ src/test/java/personal/junseok/tdd/MoneyTest.java
@@ -52,6 +52,11 @@ class MoneyTest {
         assertEquals(Money.dollar(7), result);
     }
 
-
+    @Test
+    void testReduceMoney() {
+        Bank bank = new Bank();
+        Money result = bank.reduce(Money.dollar(1), "USD");
+        assertEquals(Money.dollar(1), result);
+    }
 
 }
